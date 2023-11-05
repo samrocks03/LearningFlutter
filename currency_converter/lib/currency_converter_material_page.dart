c00bfb3825fd6c53da81e4292b831c8fed0d1d3e
Samarth Kulkarni <svkulkarni23@gmail.com>
2023-11-05 17:15:23 +0530

    Understanding components in dart

---
+++ currency_converter/lib/currency_converter_material_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConverterMaterialPage extends StatelessWidget{
+
+  const CurrencyConverterMaterialPage({super.key});
+  @override
+  Widget build(BuildContext context){
+    return const Scaffold(
+        body: Center(   
+          child: Text("Hey bru!!"),
+          )
+      );
+  }
+}
