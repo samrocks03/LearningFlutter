2b545d252f04e629f2c3a609ccaf2d5f9c957418
Samarth Kulkarni <svkulkarni23@gmail.com>
2023-11-11 14:36:57 +0530

    Modified text in Text()

--- weather_apk/lib/main.dart
+++ weather_apk/lib/main.dart
@@ -82,7 +82,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Press the BUTTon:',
             ),
             Text(
               '$_counter',
