871c5b276e25786f97202cc9502a9d70000c6f33
Samarth Kulkarni <svkulkarni23@gmail.com>
2023-11-11 23:22:41 +0530

    Completed UI of the first Main Card of our App

--- weather_apk/lib/screen.dart
+++ weather_apk/lib/screen.dart
@@ -29,19 +29,75 @@ class WeatherApp extends StatelessWidget {
       ),
 
 
-      body: const Column(
-        children: [
-          // Main card for Current Weather forecast
-          Placeholder(
-            fallbackHeight: 250,
-          ),
+      body: Padding(
+        padding:  EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            // Main card for Current Weather forecast
+            // Placeholder(
+            //   fallbackHeight: 250,
+            //   child: Text("Main Cards"),
+            // ),
+      
+          Container(
+            width: double.infinity,
+            child: Card(
+              shape : RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50) 
+                  ),  
+              elevation: 50,
 
-          SizedBox(height: 40,),
-          Placeholder(
-            fallbackHeight: 150,
+              child: const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Text("17 °F",
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                      )
+                      ),
+              
+                    SizedBox(
+                      width: 20,
+                    ),
+              
+                    Icon(
+                      Icons.water,
+                      size: 60,
+                      ),
+              
+                    SizedBox(
+                      width: 20,
+                    ),
+                    
+                    Text("Rain",
+                      style: TextStyle(
+                        fontSize: 20,
+                        
+                      )
+                      ),
+                  ],
+                ),
+              ),
+            ),
           ),
-
-        ],
+            const SizedBox(height: 40,),
+      
+            Placeholder(
+              fallbackHeight: 150,
+              child: Text("Weather Forecast Sliding Cards"),
+            ),
+      
+            SizedBox(height: 40,),
+      
+            Placeholder(
+              fallbackHeight: 150,
+              child: Text("Other Information Cards"),
+            ),
+            
+          ],
+        ),
       ),
     );
   }
