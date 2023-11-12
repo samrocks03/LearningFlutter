3b7f1120d2d4e9d0f9971971ed34738dc4a9862f
Samarth Kulkarni <svkulkarni23@gmail.com>
2023-11-13 00:37:08 +0530

    Added horizontal scroll property

--- weather_apk/lib/screen.dart
+++ weather_apk/lib/screen.dart
@@ -107,177 +107,180 @@ class WeatherApp extends StatelessWidget {
             const SizedBox(height: 14,),
 
 
-            Row(
-              children: [
-                
-                SizedBox(
-                  width: 100,
-                  child: Card(
-                    shape : RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20) 
-                      ),
-                    elevation: 50,
-                    child : Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: Column(
-                        children: [
-                          Text("3:00",
-                            style: TextStyle(
-                              fontSize: 14,
-                            )
-                            ),
-                    
-                          SizedBox(height: 5),
-                    
-                          Icon(
-                            Icons.cloud_queue_sharp,
-                            size: 30,
-                            ),
-                    
-                          SizedBox(height: 5),
-                    
-                          Text("14 °F",),
-                        ],
-                      ),
-                    )
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  
+                  SizedBox(
+                    width: 100,
+                    child: Card(
+                      shape : RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20) 
+                        ),
+                      elevation: 50,
+                      child : Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Column(
+                          children: [
+                            Text("3:00",
+                              style: TextStyle(
+                                fontSize: 14,
+                              )
+                              ),
+                      
+                            SizedBox(height: 5),
+                      
+                            Icon(
+                              Icons.cloud_queue_sharp,
+                              size: 30,
+                              ),
+                      
+                            SizedBox(height: 5),
+                      
+                            Text("14 °F",),
+                          ],
+                        ),
+                      )
+                    ),
                   ),
-                ),
-                
-                SizedBox(
-                  width: 100,
-                  child: Card(
-                    shape : RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20) 
-                      ),
-                    elevation: 50,
-                    child : Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: Column(
-                        children: [
-                          Text("6:00",
-                            style: TextStyle(
-                              fontSize: 14,
-                            )
-                            ),
-                    
-                          SizedBox(height: 5),
-                    
-                          Icon(
-                            Icons.water,
-                            size: 30,
-                            ),
-                    
-                          SizedBox(height: 5),
-                    
-                          Text("25 °F",),
-                        ],
-                      ),
-                    )
+                  
+                  SizedBox(
+                    width: 100,
+                    child: Card(
+                      shape : RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20) 
+                        ),
+                      elevation: 50,
+                      child : Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Column(
+                          children: [
+                            Text("6:00",
+                              style: TextStyle(
+                                fontSize: 14,
+                              )
+                              ),
+                      
+                            SizedBox(height: 5),
+                      
+                            Icon(
+                              Icons.water,
+                              size: 30,
+                              ),
+                      
+                            SizedBox(height: 5),
+                      
+                            Text("25 °F",),
+                          ],
+                        ),
+                      )
+                    ),
                   ),
-                ),
-                
-                SizedBox(
-                  width: 100,
-                  child: Card(
-                    shape : RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20) 
-                      ),
-                    elevation: 50,
-                    child : Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: Column(
-                        children: [
-                          Text("9:00",
-                            style: TextStyle(
-                              fontSize: 14,
-                            )
-                            ),
-                    
-                          SizedBox(height: 5),
-                    
-                          Icon(
-                            Icons.air_sharp,
-                            size: 30,
-                            ),
-                    
-                          SizedBox(height: 5),
-                    
-                          Text("14 °F",),
-                        ],
-                      ),
-                    )
+                  
+                  SizedBox(
+                    width: 100,
+                    child: Card(
+                      shape : RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20) 
+                        ),
+                      elevation: 50,
+                      child : Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Column(
+                          children: [
+                            Text("9:00",
+                              style: TextStyle(
+                                fontSize: 14,
+                              )
+                              ),
+                      
+                            SizedBox(height: 5),
+                      
+                            Icon(
+                              Icons.air_sharp,
+                              size: 30,
+                              ),
+                      
+                            SizedBox(height: 5),
+                      
+                            Text("14 °F",),
+                          ],
+                        ),
+                      )
+                    ),
                   ),
-                ),
-                
-                SizedBox(
-                  width: 100,
-                  child: Card(
-                    shape : RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20) 
-                      ),
-                    elevation: 50,
-                    child : Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: Column(
-                        children: [
-                          Text("12:00",
-                            style: TextStyle(
-                              fontSize: 14,
-                            )
-                            ),
-                    
-                          SizedBox(height: 5),
-                    
-                          Icon(
-                            Icons.cloud_queue_sharp,
-                            size: 30,
-                            ),
-                    
-                          SizedBox(height: 5),
-                    
-                          Text("19 °F",),
-                        ],
-                      ),
-                    )
+                  
+                  SizedBox(
+                    width: 100,
+                    child: Card(
+                      shape : RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20) 
+                        ),
+                      elevation: 50,
+                      child : Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Column(
+                          children: [
+                            Text("12:00",
+                              style: TextStyle(
+                                fontSize: 14,
+                              )
+                              ),
+                      
+                            SizedBox(height: 5),
+                      
+                            Icon(
+                              Icons.cloud_queue_sharp,
+                              size: 30,
+                              ),
+                      
+                            SizedBox(height: 5),
+                      
+                            Text("19 °F",),
+                          ],
+                        ),
+                      )
+                    ),
                   ),
-                ),
-                
-                SizedBox(
-                  width: 100,
-                  child: Card(
-                    shape : RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20) 
-                      ),
-                    elevation: 50,
-                    child : Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: Column(
-                        children: [
-                          Text("3:00",
-                            style: TextStyle(
-                              fontSize: 14,
-                            )
-                            ),
-                    
-                          SizedBox(height: 5),
-                    
-                          Icon(
-                            Icons.water,
-                            size: 30,
-                            ),
-                    
-                          SizedBox(height: 5),
-                    
-                          Text("12 °F",),
-                        ],
-                      ),
-                    )
+                  
+                  SizedBox(
+                    width: 100,
+                    child: Card(
+                      shape : RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20) 
+                        ),
+                      elevation: 50,
+                      child : Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Column(
+                          children: [
+                            Text("3:00",
+                              style: TextStyle(
+                                fontSize: 14,
+                              )
+                              ),
+                      
+                            SizedBox(height: 5),
+                      
+                            Icon(
+                              Icons.water,
+                              size: 30,
+                              ),
+                      
+                            SizedBox(height: 5),
+                      
+                            Text("12 °F",),
+                          ],
+                        ),
+                      )
+                    ),
                   ),
-                ),
-
-
-
-              ],
+            
+            
+            
+                ],
+              ),
             ),
 
             SizedBox(height: 40,),
