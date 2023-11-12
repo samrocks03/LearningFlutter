350f7347e0bbd507016c5340a98c424725c197c7
Samarth Kulkarni <svkulkarni23@gmail.com>
2023-11-13 00:54:47 +0530

    Replaced repeating code with widgets

--- weather_apk/lib/screen.dart
+++ weather_apk/lib/screen.dart
@@ -114,140 +114,14 @@ class WeatherApp extends StatelessWidget {
                   
                   HourlyForecastWidget(),
                   
-                  SizedBox(
-                    width: 100,
-                    child: Card(
-                      shape : RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20) 
-                        ),
-                      elevation: 50,
-                      child : Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: Column(
-                          children: [
-                            Text("6:00",
-                              style: TextStyle(
-                                fontSize: 14,
-                              )
-                              ),
-                      
-                            SizedBox(height: 5),
-                      
-                            Icon(
-                              Icons.water,
-                              size: 30,
-                              ),
-                      
-                            SizedBox(height: 5),
-                      
-                            Text("25 °F",),
-                          ],
-                        ),
-                      )
-                    ),
-                  ),
+                  HourlyForecastWidget(),
+
+                  HourlyForecastWidget(),
                   
-                  SizedBox(
-                    width: 100,
-                    child: Card(
-                      shape : RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20) 
-                        ),
-                      elevation: 50,
-                      child : Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: Column(
-                          children: [
-                            Text("9:00",
-                              style: TextStyle(
-                                fontSize: 14,
-                              )
-                              ),
-                      
-                            SizedBox(height: 5),
-                      
-                            Icon(
-                              Icons.air_sharp,
-                              size: 30,
-                              ),
-                      
-                            SizedBox(height: 5),
-                      
-                            Text("14 °F",),
-                          ],
-                        ),
-                      )
-                    ),
-                  ),
+                  HourlyForecastWidget(),
                   
-                  SizedBox(
-                    width: 100,
-                    child: Card(
-                      shape : RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20) 
-                        ),
-                      elevation: 50,
-                      child : Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: Column(
-                          children: [
-                            Text("12:00",
-                              style: TextStyle(
-                                fontSize: 14,
-                              )
-                              ),
-                      
-                            SizedBox(height: 5),
-                      
-                            Icon(
-                              Icons.cloud_queue_sharp,
-                              size: 30,
-                              ),
-                      
-                            SizedBox(height: 5),
-                      
-                            Text("19 °F",),
-                          ],
-                        ),
-                      )
-                    ),
-                  ),
-                  
-                  SizedBox(
-                    width: 100,
-                    child: Card(
-                      shape : RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20) 
-                        ),
-                      elevation: 50,
-                      child : Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: Column(
-                          children: [
-                            Text("3:00",
-                              style: TextStyle(
-                                fontSize: 14,
-                              )
-                              ),
-                      
-                            SizedBox(height: 5),
-                      
-                            Icon(
-                              Icons.water,
-                              size: 30,
-                              ),
-                      
-                            SizedBox(height: 5),
-                      
-                            Text("12 °F",),
-                          ],
-                        ),
-                      )
-                    ),
-                  ),
-            
-            
-            
+                  HourlyForecastWidget(),
+                              
                 ],
               ),
             ),
