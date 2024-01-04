7ae08e20041223b36559a9afd62c124196b935d5
Samarth Kulkarni <svkulkarni23@gmail.com>
2024-01-04 18:01:55 +0530

    Added product data files

---
+++ shop_app/lib/global_variables.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+
+// This list will contain map of String:Dynamic type
+
+final products = [
+  {
+    'id': '0',
+    'title': 'Men\'s Nike Shoes',
+    'price': 44.52,
+    'imageUrl': 'assets/images/shoes_1.png',
+    'company': 'Nike',
+    'sizes': [9, 10, 11, 12],
+  },
+  {
+    'id': '1',
+    'title': 'Adidas Shoes',
+    'price': 20.12,
+    'imageUrl': 'assets/images/shoes_2.png',
+    'company': 'Adidas',
+    'sizes': [9, 10, 12],
+  },
+  {
+    'id': '2',
+    'title': 'Bata Women\'s Shoes',
+    'price': 28.95,
+    'imageUrl': 'assets/images/shoes_3.png',
+    'company': 'Bata',
+    'sizes': [8, 9, 10],
+  },
+  {
+    'id': '3',
+    'title': 'Jordan Shoes',
+    'price': 420.69,
+    'imageUrl': 'assets/images/shoes_4.png',
+    'company': 'Nike',
+    'sizes': [8, 9, 10],
+  },
+];
