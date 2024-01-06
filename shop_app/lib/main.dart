daac74a5752a68c7213dbee18b12fc9f41e456f1
Samarth Kulkarni <svkulkarni23@gmail.com>
2024-01-06 12:59:39 +0530

    Theme updated

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -1,69 +1,87 @@
 // ignore_for_file: prefer_const_constructors
+// main.dart
+// import 'package:shop_app/cart_page.dart';
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:shop_app/cart_page.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/cart_provider.dart';
 import 'package:shop_app/home_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+
+// ignore: slash_for_doc_comments
+/**
+ * Types of Provider:
+ *  - Provider                      --> Read Only value
+ *  - ChangeNotifier Provider       --> Change the value, notify the widgets that are listening to it about their changes.
+ *  - Future Provider               --> 
+ *  - Stream Provider
+ *                                    Future and Stream provider help in doing lazy loaders task
+ */
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
+  // build func
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          primary: Color.fromRGBO(254, 206, 1, 1),
-          secondary: Color.fromRGBO(245, 247, 249, 1),
-          seedColor: Color.fromRGBO(254, 206, 1, 1)
-        ),
-
-        inputDecorationTheme: InputDecorationTheme(
-          hintStyle: GoogleFonts.abhayaLibre(
-            fontSize: 15
+    return ChangeNotifierProvider(
+      create: (context) => CartProvider(),
+      child: MaterialApp(
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(
+            primary: Color.fromRGBO(254, 206, 1, 1),
+            secondary: Color.fromRGBO(245, 247, 249, 1),
+            seedColor: Color.fromRGBO(254, 206, 1, 1)
           ),
-          prefixIconColor: Color.fromRGBO(119,119,119,1)
-        ),
-
-        appBarTheme: AppBarTheme(
-          titleTextStyle: GoogleFonts.aBeeZee(
-            fontSize: 25,
-            color: Colors.black
+      
+          inputDecorationTheme: InputDecorationTheme(
+            hintStyle: GoogleFonts.abhayaLibre(
+              fontSize: 15
+            ),
+            prefixIconColor: Color.fromRGBO(119,119,119,1)
+          ),
+      
+          appBarTheme: AppBarTheme(
+            titleTextStyle: GoogleFonts.aBeeZee(
+              fontSize: 25,
+              color: Colors.black
+            )
+          ),
+      
+          textTheme: TextTheme(
+            titleMedium: GoogleFonts.aBeeZee(
+              fontWeight: FontWeight.bold,
+              fontSize: 20
+            ),
+      
+            bodyMedium:GoogleFonts.aBeeZee(
+              fontWeight: FontWeight.bold,
+              fontSize: 24
+            ),
+      
+            bodySmall:GoogleFonts.aBeeZee(
+              fontWeight: FontWeight.bold,
+              fontSize: 15
+            ),
+      
+            titleLarge: GoogleFonts.aBeeZee(
+                  textStyle: TextStyle(
+                      color: Colors.blue.shade900,
+                      letterSpacing: .3,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w900
+                    ),
+              ),
           )
         ),
-
-        textTheme: TextTheme(
-          titleMedium: GoogleFonts.aBeeZee(
-            fontWeight: FontWeight.bold,
-            fontSize: 20
-          ),
-
-          bodyMedium:GoogleFonts.aBeeZee(
-            fontWeight: FontWeight.bold,
-            fontSize: 24
-          ),
-
-          titleLarge: GoogleFonts.aBeeZee(
-                textStyle: TextStyle(
-                    color: Colors.blue.shade900,
-                    letterSpacing: .3,
-                    fontSize: 30,
-                    fontWeight: FontWeight.w900),
-              ),          
-        ),
-
-        iconTheme: IconThemeData(
-          size: 19.0
-        )
+        debugShowCheckedModeBanner: false,
+        home: const HomePage(),
       ),
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
     );
   }
 }
